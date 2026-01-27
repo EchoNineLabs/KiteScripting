36c3d0a786c16dc9251c75a8298edd8f6defb156
Michał <michal.czopek.foss@proton.me>
2026-01-28 00:58:36 +0100

    Kite 1.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     // Kite dependency is required for .kite.kts scripts to be properly recognized.
-    compileOnly("dev.echonine.kite:kite:1.1.0")
+    compileOnly("dev.echonine.kite:kite:1.2.1")
     // Server API can be Paper, Folia, Purpur or any other compatible fork.
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     // Other dependencies used in scripts like e.g., LuckPerms or PlaceholderAPI, can be added as well.
