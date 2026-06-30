2732b6d6cf36b3ae297e16a5e737dd4d117cfd31
Michał <michal.czopek.foss@proton.me>
2026-07-01 01:29:33 +0200

    Kite 1.6.0, Kotlin 2.4.10-RC, Paper 26.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.4.0"
+    id("org.jetbrains.kotlin.jvm") version "2.4.10-RC"
 }
 
 repositories {
@@ -11,8 +11,8 @@ repositories {
 
 dependencies {
     // Kite dependency is required for .kite.kts scripts to be properly recognized.
-    compileOnly("dev.echonine:kite:1.5.2")
+    compileOnly("dev.echonine:kite:1.6.0")
     // Server API can be Paper, Folia, Purpur or any other compatible fork.
-    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
     // Other dependencies used in scripts like e.g., LuckPerms or PlaceholderAPI, can be added as well.
 }
